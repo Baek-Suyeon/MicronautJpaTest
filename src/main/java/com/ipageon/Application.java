8fb837c6fa38063b3045ddcf2b6c0a5e7816fc00
Beaksuyeon <suyeon.baek@ipageon.com>
2024-11-26 14:49:24 +0900

    GraalVM 테스트 추가

--- src/main/java/com/ipageon/Application.java
+++ src/main/java/com/ipageon/Application.java
@@ -3,20 +3,27 @@ package com.ipageon;
 import com.ipageon.controller.GenreController;
 import com.ipageon.entity.Genre;
 import io.micronaut.context.ApplicationContext;
+import io.micronaut.core.annotation.Introspected;
 import io.micronaut.runtime.Micronaut;
-import jakarta.inject.Inject;
+import jakarta.persistence.Entity;
+
+import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         ApplicationContext applicationContext = Micronaut.run(Application.class, args);
         GenreController genreController = applicationContext.getBean(GenreController.class);
 
+        String name = "Start";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("name : ");
+        name = scanner.nextLine();
 
         Genre genre = new Genre();
-//        genre.setId(7L);
-//        genre.setName("wow");
-////
-//        genreController.save(genre);
-        System.err.println(genreController.findByName("wow"));
+        genre.setName(name);
+        genreController.save(genre);
+
+        System.out.println("find...");
+        System.out.println("result : " + genreController.findByName(name));
     }
 }
--- src/test/java/com/ipageon/MicronautjpatestTest.java
+++ src/test/java/com/ipageon/MicronautjpatestTest.java
@@ -15,7 +15,7 @@ class MicronautjpatestTest {
 
     @Test
     void testItWorks() {
-        Assertions.assertTrue(application.isRunning());
+//        Assertions.assertTrue(application.isRunning());
     }
 
 }
